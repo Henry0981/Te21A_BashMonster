1dc8fff1a170f8292b47ceee7219d85a9e50d8b7
Henry Gillinger <henry.gillinger@ga.ntig.se>
2023-09-01 11:37:44 +0200

    first commit

---
+++ Te21A_BashMonster/Program.cs
@@ -0,0 +1,11 @@
+﻿using asda;
+
+fish Wanda = new fish();
+fish henry = new fish();
+
+Wanda.Damage = 100;
+henry.Damage = 200;
+
+Console.WriteLine(Wanda.Damage);
+
+Console.ReadLine(); 
---
+++ Te21A_BashMonster/fish.cs
@@ -0,0 +1,8 @@
+namespace asda;
+public class fish
+{
+   public bool IsEdible;
+   public int Damage;
+    public float Weight;
+    public bool Stackable;
+}
